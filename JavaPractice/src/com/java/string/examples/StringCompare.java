09ca52ea8722738bc5a8a0aa648f708bdd2210c3
Abhi <Abhi@Abhi-PC>
2017-06-06 16:28:30 +0530

    Hi

--- JavaPractice/src/com/java/string/examples/StringCompare.java
+++ JavaPractice/src/com/java/string/examples/StringCompare.java
@@ -4,6 +4,19 @@ public class StringCompare {
 	
 	public static void main(String[] args) {
 		
+		String str = "Hello World";
+		String str2 = "hello world";
+		Object strObj = str;
+		
+		
+		System.out.println(str.compareTo(str2));
+		
+		System.out.println(str.compareToIgnoreCase(str2));
+		
+		System.out.println(str.compareTo((String) strObj));
+		
+		
 	}
 
 }
+	
--- JavaPractice/src/com/java/string/examples/WordCount1.java
+++ JavaPractice/src/com/java/string/examples/WordCount1.java
@@ -1,6 +1,6 @@
 package com.java.string.examples;
 
-public class WordCount {
+public class WordCount1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
